b8928c330987f281c6221b5d72ecd16caad21239
akella <akella.a@gmail.com>
2013-08-29 16:20:01 +0300

    pairs done

--- js/common.js
+++ js/common.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+
+		pane = $('.pairs__scroll');
+		pane.jScrollPane({
+		  	autoReinitialise: true,
+		  	hideFocus: true,
+		  	animateTo: true
+		});
+		var api = pane.data('jsp');
+
+	
 });
